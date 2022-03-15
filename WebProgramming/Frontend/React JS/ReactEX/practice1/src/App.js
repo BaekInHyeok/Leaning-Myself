ebfd7c61e9781e52f70198771fc04cde7e05ddfc
BaekInHyeok <bih63879@gmail.com>
2022-03-15 22:49:58 +0900

    React for Biginners 10.ReactApp useEffect

--- WebProgramming/Frontend/React JS/ReactEX/practice1/src/App.js
+++ WebProgramming/Frontend/React JS/ReactEX/practice1/src/App.js
@@ -5,9 +5,12 @@ import { useState,useEffect } from 'react';
 function App() {
   const[counter,setValue]=useState(0);
   const[keyword,setKeyword]=useState("");
+
   const onClick=()=>setValue((prev) => prev+1)
   const onChange=(event)=>setKeyword(event.target.value)
+
   console.log("I run all the time");
+
   const IRunOnlyOnce=()=>{
     console.log("I run only once")
   }
@@ -15,17 +18,19 @@ function App() {
   useEffect(IRunOnlyOnce,[]);//코드를 단 한번만 실행하도록 해준다
 
   useEffect(()=>{
-    console.log("I run when keyword is changed",keyword)
+    console.log("I run when keyword is changed")
   },[keyword]);//[]부분에 코드가 실행되는 변화가 적용되는 곳을 입력한다
 
   useEffect(()=>{
-    console.log("I run when counter is changed",counter)
+    console.log("I run when counter is changed")
   },[counter]);
+
+  useEffect(()=>{
+    console.log("I run when keyword & counter are changed")
+  },[keyword,counter])
+
   return (
     <div>
-      <h1>Welcome</h1>
-      <hr />
-
       <input 
       value={keyword}
       onChange={onChange} 
