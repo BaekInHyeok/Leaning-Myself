782d8860077feeec431737a737fcfb80968f6171
BaekInHyeok <bih63879@gmail.com>
2022-07-17 23:02:29 +0900

    보험코드(EDI Code)를 이용한 약품 정보 검색을 위한 공공데이터 Open API 활용 기능 연습

---
+++ AndroidProgramming/OpenAPITest3/app/src/main/java/com/cookandroid/openapitest3/MainActivity.java
@@ -0,0 +1,116 @@
+package com.cookandroid.openapitest3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Xml;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit;
+    TextView text;
+    XmlPullParser xpp;
+
+    String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("보험코드검색");
+
+        edit=(EditText) findViewById(R.id.edit);
+        text=(TextView) findViewById(R.id.result);
+    }
+
+    public void mOnClick(View v){
+        switch(v.getId()){
+            case R.id.button:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        data=getXmlData();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(data);
+                            }
+                        });
+                    }
+                }).start();
+                break;
+        }
+    }
+
+    String getXmlData(){
+        StringBuffer buffer=new StringBuffer();
+        String str=edit.getText().toString();
+        String medicName=URLEncoder.encode(str);
+        String query="%EC%A0%84%EB%A0%A5%EB%A1%9C";
+
+
+        String queryUrl="http://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService02/getDrugPrdtPrmsnDtlInq01?serviceKey=RZnyfUGsOhY2tWWUv262AHpeMQYn4Idqd5cgG0rGNHPd648m5j0Pu3eiS3ewN4XhhHT%2FvuliAmF9KLJdzh1TFA%3D%3D&pageNo=1&numOfRows=3&type=xml&edi_code="+medicName;
+        try {
+            URL url=new URL(queryUrl);
+            InputStream is=url.openStream();
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp=factory.newPullParser();
+            xpp.setInput(new InputStreamReader(is,"UTF-8"));
+
+            String tag;
+
+            xpp.next();
+            int eventType=xpp.getEventType();
+
+            while(eventType != XmlPullParser.END_DOCUMENT){
+                switch (eventType){
+                    /*case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작");
+                        break;*/
+
+                    case XmlPullParser.START_TAG:
+                        tag=xpp.getName();
+
+                        if(tag.equals("item"));
+                        else if(tag.equals("ITEM_NAME")){
+                            buffer.append("품목명 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        /*else if(tag.equals("ITEM_SEQ")){
+                            buffer.append("품목기준코드 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }*/
+                        break;
+                    /*case XmlPullParser.TEXT:
+                        break;
+                    case XmlPullParser.END_TAG:
+                        tag=xpp.getName();
+
+                        if(tag.equals("item"))buffer.append("\n");*/
+                }
+                eventType=xpp.next();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        /*buffer.append("파싱 끝\n");*/
+        return buffer.toString();
+
+    }
+}
